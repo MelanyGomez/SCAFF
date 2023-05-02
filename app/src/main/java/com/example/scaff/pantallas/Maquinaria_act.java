a00d4f257a52bdb5724fb4d78d6cebfd7e422473
Melany Gomez <go404756@uaeh.edu.mx>
2023-05-01 23:28:11 -0500

    barra superior

--- app/src/main/java/com/example/scaff/pantallas/Maquinaria_act.java
+++ app/src/main/java/com/example/scaff/pantallas/Maquinaria_act.java
@@ -2,9 +2,12 @@ package com.example.scaff.pantallas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.scaff.R;
+import com.example.scaff.Registro_materiales;
 
 public class Maquinaria_act extends AppCompatActivity {
 
@@ -13,4 +16,5 @@ public class Maquinaria_act extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maquinaria);
     }
+
 }
