2ee68bc5b0e65d17bc4958809cc39742f1eb9629
Carlos <sa476536@uaeh.edu.mx>
2023-05-01 22:35:05 -0600

    Primer commit

--- app/src/main/java/com/example/scaff/ConexionSQLiteHelper.java
+++ app/src/main/java/com/example/scaff/ConexionSQLiteHelper.java
@@ -1,10 +1,14 @@
 package com.example.scaff;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
 
 import androidx.annotation.Nullable;
+
+import com.example.scaff.entidades.Maquina;
 
 public class ConexionSQLiteHelper extends SQLiteOpenHelper {
 
@@ -14,11 +18,86 @@ public class ConexionSQLiteHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + "maquinas" + " ("
+                + MaquinaEntry.ID_MAQUINA + " integer primary key autoincrement,"
+                + MaquinaEntry.NOMBRE + " text not null,"
+                + MaquinaEntry.DESCRIP + " text not null,"
+                + MaquinaEntry.CLIENTE + " text not null,"
+                + MaquinaEntry.MESES + "text not null,"
+                + "UNIQUE (" + MaquinaEntry.ID_MAQUINA + "))");
+
+        mockData(db);
 
     }
+    private void mockData(SQLiteDatabase db) {
+    }
+    public static abstract class MaquinaEntry implements BaseColumns {
+        public static final String TABLE_NAME ="Maquina";
 
+        public static final String ID_MAQUINA = "id_maquina";
+        public static final String NOMBRE = "nombre";
+        public static final String DESCRIP = "descrip";
+        public static final String CLIENTE = "cliente";
+        public static final String MESES = "meses";
+
+    }
+    public long mockMaquina(SQLiteDatabase db, Maquina maquina) {
+        return db.insert(
+                MaquinaEntry.TABLE_NAME,
+                null,
+                maquina.toContentValues());
+    }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+    public long saveMaquina(Maquina maquina) {
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+
+        return sqLiteDatabase.insert(
+                MaquinaEntry.TABLE_NAME,
+                null,
+                maquina.toContentValues());
+
+    }
+
+    public Cursor getAllMaquinas() {
+        return getReadableDatabase()
+                .query(
+                        MaquinaEntry.TABLE_NAME,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null);
+    }
+
+    public Cursor getMaquinaById(String maquinaId) {
+        Cursor c = getReadableDatabase().query(
+                MaquinaEntry.TABLE_NAME,
+                null,
+                MaquinaEntry.ID_MAQUINA + "",
+                new String[]{maquinaId},
+                null,
+                null,
+                null);
+        return c;
+    }
+
+    public int deleteMaquina(String maquinaId) {
+        return getWritableDatabase().delete(
+                MaquinaEntry.TABLE_NAME,
+                MaquinaEntry.ID_MAQUINA + "",
+                new String[]{maquinaId});
+    }
+
+    public int updateMaquina(Maquina maquina, String maquinaId) {
+        return getWritableDatabase().update(
+                MaquinaEntry.TABLE_NAME,
+                maquina.toContentValues(),
+                MaquinaEntry.ID_MAQUINA + "",
+                new String[]{maquinaId}
+        );
     }
 }
