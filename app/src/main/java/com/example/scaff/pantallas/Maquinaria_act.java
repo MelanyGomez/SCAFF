651808cff83a537a8f4b092ea7df25ffe41cb865
Carlos <sa476536@uaeh.edu.mx>
2023-05-02 00:17:21 -0600

    Maquinaria

--- app/src/main/java/com/example/scaff/pantallas/Maquinaria_act.java
+++ app/src/main/java/com/example/scaff/pantallas/Maquinaria_act.java
@@ -5,16 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 import com.example.scaff.R;
 import com.example.scaff.Registro_materiales;
 
 public class Maquinaria_act extends AppCompatActivity {
+ListView lv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maquinaria);
+        lv1= (ListView) findViewById(R.id.lv1);
+
     }
 
 }
